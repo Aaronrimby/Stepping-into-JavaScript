49382efdf8788d85096c38b0c7c0064fff79edf6
aaronrimby <aaronrimby@gmail.com>
2018-02-11 12:26:28 -0500

    code challenge of averaging 5 numbers

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-function getAverage (a,b) {
+function getAverage (a,b,c,d,e) {
 
-    var average = (a + b)/2;
+    var average = (a + b + c + d + e)/5;
     console.log(average);
     return average;
 }
 
-var myResult = getAverage(7,8);
+var myResult = getAverage(7,8, 100, 826, 1);
 console.log("my result is " + myResult);
