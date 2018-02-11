ce72644d970a16f9f7040fde23f8e1684e795b48
aaronrimby <aaronrimby@gmail.com>
2018-02-11 13:39:52 -0500

    added drink.js to check for drinking age

---
+++ drink.js
@@ -0,0 +1,16 @@
+function canDrink(a){
+
+     if(a < 1997){
+
+          console.log("you can drink");
+          document.write("what can I get ya?");
+     }else {
+          console.log("you cannot drink");
+          document.write("get outta here kid");
+     }
+
+
+
+}
+
+canDrink(1978);
